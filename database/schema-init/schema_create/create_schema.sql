96a17dcaab99d64dbdad2f6e3d5bf3f7be57282f
Bradly Hudson <bhudson@Bradly-Hudson-MacBook-Pro32GB.local>
2019-02-28 08:18:38 -0600

    Added comments to script

--- database/schema-init/schema_create/create_schema.sql
+++ database/schema-init/schema_create/create_schema.sql
@@ -1,5 +1,8 @@
+--Create admin user and schema for OCM
 CREATE USER ocmadmin WITH PASSWORD 'ocmadmin';
 CREATE SCHEMA ocm AUTHORIZATION ocmadmin;
+--We don't want users connecting as admin, so we'll create a user account
+--with limited privs.
 CREATE USER ocmuser WITH PASSWORD 'ocmuser';
 CREATE ROLE ocm_user_r;
 GRANT SELECT ON ALL TABLES IN SCHEMA ocm TO ocm_user_r;
